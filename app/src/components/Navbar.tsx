2032ab3ef462e7218290d9a84930ce4bbae5ddac
michaelyfu <m888fu@gmail.com>
2023-12-07 14:21:50 -0500

    need to finish building accessibility bar

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -1,35 +1,40 @@
 import React from "react";
+import AccessibilityBar from "./AccessibilityBar";
 type NavBarProps = {
   onChange: (value: string) => void;
 };
 function NavBar({ onChange }: NavBarProps) {
   return (
-    <ul className="h-14 w-full items-center justify-center bg-gray-200 flex gap-4">
-      <li>
-        <div
-          className="hover:cursor-pointer hover:text-purple-600 transition-all duration-150"
-          onClick={(e) => onChange("all")}
-        >
-          All
-        </div>
-      </li>
-      <li>
-        <div
-          className="hover:cursor-pointer hover:text-purple-600 transition-all duration-150"
-          onClick={(e) => onChange("fiction")}
-        >
-          Fiction
-        </div>
-      </li>
-      <li>
-        <div
-          className="hover:cursor-pointer hover:text-purple-600 transition-all duration-150"
-          onClick={(e) => onChange("nonfiction")}
-        >
-          Nonfiction
-        </div>
-      </li>
-    </ul>
+    <>
+      <ul className="h-14 w-full items-center justify-center bg-gray-200 flex gap-4">
+        <li>
+          <div
+            className="hover:cursor-pointer hover:text-purple-600 transition-all duration-150"
+            onClick={(e) => onChange("all")}
+          >
+            All
+          </div>
+        </li>
+        <li>
+          <div
+            className="hover:cursor-pointer hover:text-purple-600 transition-all duration-150"
+            onClick={(e) => onChange("fiction")}
+          >
+            Fiction
+          </div>
+        </li>
+        <li>
+          <div
+            className="hover:cursor-pointer hover:text-purple-600 transition-all duration-150"
+            onClick={(e) => onChange("nonfiction")}
+          >
+            Nonfiction
+          </div>
+        </li>
+      </ul>
+
+      <AccessibilityBar />
+    </>
   );
 }
 
