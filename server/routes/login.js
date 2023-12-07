34c80a0d8b591a51c74e0f2dc686daae86a7d4fe
azhou76 <alex.z.zhou21@gmail.com>
2023-12-07 13:28:39 -0500

    feat: backend

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,3 +1,68 @@
+// // // Load HTTP module
+// // const express = require("express");
+// // const app = express();
+// // const port = 8000;
+
+// // app.get("/signUp", (req, res) => {
+// //   res.send("Hello, World!");
+// // });
+
+// // app.listen(port, () => {
+// //   console.log(`Server is running at http://localhost:${port}`);
+// // });
+
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const User = require("./user"); // Import your Mongoose model
+
+// const app = express.Router();
+// const port = 3000;
+
+// mongoose.connect("mongodb://localhost/your-database-name", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// const db = mongoose.connection;
+
+// db.on("error", console.error.bind(console, "MongoDB connection error:"));
+// db.once("open", () => {
+//   console.log("Connected to MongoDB");
+// });
+
+// // Create a new user
+// app.post("/signup", async (req, res) => {
+//   const { username, email } = req.body;
+
+//   try {
+//     const newUser = new User({ username, email });
+//     await newUser.save();
+//     res.json(newUser);
+//   } catch (error) {
+//     res.status(500).json({ error: "Error creating user" });
+//   }
+// });
+
+// // Get all users
+// app.get("/login", async (req, res) => {
+//   const { username, email } = req.body;
+
+//   try {
+//     const users = await User.findOne({ email: email });
+//     if (users) {
+//       return res.json(users);
+//     }
+//   } catch (error) {
+//     res.status(500).json({ error: "Error fetching users" });
+//   }
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running at http://localhost:${port}`);
+// });
+
+// push 1
+
 import bcrypt from "bcrypt";
 
 const express = require("express");
@@ -45,15 +110,23 @@ recordRoutes.route("/signIn").get(async function (req, res) {
     if (!records) {
       return res.status(401).json({ message: "Email incorrect" });
     } else {
-      const passwordCorrect = await bcrypt.compare(password, records.password);
+      try {
+        const passwordCorrect = await bcrypt.compare(
+          password,
+          records.password
+        );
 
-      if (!passwordCorrect) {
-        return res
-          .status(401)
-          .json({ message: "Username or Password Incorrect" });
+        if (!passwordCorrect) {
+          return res
+            .status(401)
+            .json({ message: "Username or Password Incorrect" });
+        }
+
+        res.status(200).json({ message: "Successfully logged in" });
+      } catch (error) {
+        console.error("Error comparing passwords:", error);
+        res.status(500).json({ error: "Internal Server Error" });
       }
-
-      res.status(201).json({ message: "Successfully logged in" });
     }
   } catch (e) {
     console.log("An error occurred pulling the user. " + e);
