c753e6d355a3692ed4fe6e38e49784af2ac7b09d
michaelyfu <m888fu@gmail.com>
2023-12-07 14:36:09 -0500

    changes

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -1,40 +1,35 @@
 import React from "react";
-import AccessibilityBar from "./AccessibilityBar";
 type NavBarProps = {
   onChange: (value: string) => void;
 };
 function NavBar({ onChange }: NavBarProps) {
   return (
-    <>
-      <ul className="h-14 w-full items-center justify-center bg-gray-200 flex gap-4">
-        <li>
-          <div
-            className="hover:cursor-pointer hover:text-purple-600 transition-all duration-150"
-            onClick={(e) => onChange("all")}
-          >
-            All
-          </div>
-        </li>
-        <li>
-          <div
-            className="hover:cursor-pointer hover:text-purple-600 transition-all duration-150"
-            onClick={(e) => onChange("fiction")}
-          >
-            Fiction
-          </div>
-        </li>
-        <li>
-          <div
-            className="hover:cursor-pointer hover:text-purple-600 transition-all duration-150"
-            onClick={(e) => onChange("nonfiction")}
-          >
-            Nonfiction
-          </div>
-        </li>
-      </ul>
-
-      <AccessibilityBar />
-    </>
+    <ul className="h-14 w-full items-center justify-center bg-gray-200 flex gap-4">
+      <li>
+        <div
+          className="hover:cursor-pointer hover:text-purple-600 transition-all duration-150"
+          onClick={(e) => onChange("all")}
+        >
+          All
+        </div>
+      </li>
+      <li>
+        <div
+          className="hover:cursor-pointer hover:text-purple-600 transition-all duration-150"
+          onClick={(e) => onChange("fiction")}
+        >
+          Fiction
+        </div>
+      </li>
+      <li>
+        <div
+          className="hover:cursor-pointer hover:text-purple-600 transition-all duration-150"
+          onClick={(e) => onChange("nonfiction")}
+        >
+          Nonfiction
+        </div>
+      </li>
+    </ul>
   );
 }
 
