6bdf1229a63874133f444e78a447d33c6e12549c
frankchiu12 <si-un_chiu@brown.edu>
2023-12-15 13:46:04 -0500

    more tests

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -20,26 +20,84 @@ describe('read.js route tests', () => {
         await client.close();
     });
 
-    it('should return "Both bookId and chapter are required"', async () => {
+    it('getbook should return "Both bookId and chapter are required"', async () => {
         const response = await supertest(testApp).get('/getbook');
         expect(response.status).to.equal(400);
         expect(response.body.message).to.equal('Both bookId and chapter are required');
     });
 
-    it('should return "Nonexistent book/author"', async () => {
+    it('getbook should return "Nonexistent book/author"', async () => {
         const response = await supertest(testApp).get('/getbook?bookId=8&chapter=1');
         expect(response.status).to.equal(401);
         expect(response.body.message).to.equal('Nonexistent book/author');
     });
 
-    it('should return "Chapter not found"', async () => {
+    it('getbook should return "Chapter not found"', async () => {
         const response = await supertest(testApp).get('/getbook?bookId=2&chapter=9');
         expect(response.status).to.equal(401);
         expect(response.body.message).to.equal('Chapter not found');
     });
 
-    it('should return book details', async () => {
+    it('getbook should return book details', async () => {
         const response = await supertest(testApp).get('/getbook?bookId=1&chapter=chapter1');
         expect(response.status).to.equal(200);
+    });
+
+    it('searchbook should return an error for missing search parameters', async () => {
+        const response = await supertest(recordRoutes)
+        .get('/searchbook')
+        .expect(400);
+        expect(response.body).to.be.an('object');
+        expect(response.body).to.have.property('message', 'Both bookId and pat are required');
+    });
+
+    it('searchbook should return matches for a given pattern in a book', async () => {
+        const response = await supertest(recordRoutes)
+        .get('/searchbook?bookId=2&pat=seldom')
+        .expect(200);
+        expect(response.body).to.be.an('object');
+        expect(response.body).to.have.property('1');
+        expect(response.body['1']).to.be.an('array');
+    });
+
+    it('searchbook should return "Both bookId and pat are required"', async () => {
+        const response = await supertest(testApp).get('/searchbook?bookId=1');
+        expect(response.status).to.equal(400);
+        expect(response.body.message).to.equal('Both bookId and pat are required');
+    });
+
+    it('searchbook should return "Both bookId and pat are required"', async () => {
+        const response = await supertest(testApp).get('/searchbook?bookId=10&pat=10');
+        expect(response.status).to.equal(401);
+        expect(response.body.message).to.equal('Nonexistent book/author');
+    });
+
+    it('getbooks should return all results', async () => {
+        const response = await supertest(testApp).get('/api/getbooks');
+        expect(response.status).to.equal(200);
+    });
+
+    it('updatefavorited should return "No document matches the provided query"', async () => {
+        const response = await supertest(testApp).put('/api/updatefavorited?bookID=10');
+        expect(response.status).to.equal(404);
+    });
+
+    it('updatefavorited should return a success message', async () => {
+        const req = {
+            bookID: '1',
+            newFavoriteStatus: false
+        };
+        const response = await supertest(testApp)
+            .put('/api/updatefavorited')
+            .set('content-type', 'application/json')
+            .send(JSON.stringify(req));
+        expect(response.status).to.equal(200);
+        expect(response.text).to.equal('Document updated successfully.');
+    });
+
+    it('wrong endpoint', async () => {
+        const response = await supertest(recordRoutes)
+        .get('/wrongendpoint')
+        .expect(404);
     });
 });
