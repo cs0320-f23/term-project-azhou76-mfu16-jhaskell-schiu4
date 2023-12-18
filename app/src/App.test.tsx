602e0fa163752b30573896129a7a3cd604327a26
Jakobi Haskell <jakobi_haskell@brown.edu>
2023-12-18 16:34:09 -0500

    adds some mock testing (IS_MOCKING_DATA)

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import App from "./App";
+import { mockedData } from "./data/mockedData";
+const IS_MOCKING_DATA = true;
+test("renders nav bar text", async () => {
+  render(<App IS_MOCKING_DATA={IS_MOCKING_DATA} />);
+  await waitFor(() => {
+    // check for all, favorites, Fiction, Nonfiction
+    const textsToCheck = ["All", "Favorites", "Fiction", "Nonfiction"];
 
-test('renders learn react link', () => {
-  render(<App IS_MOCKING_DATA={false} />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    // Loop through the array and check each text
+    textsToCheck.forEach(text => {
+      console.log(new RegExp(text, "i"));
+      const element = screen.getByText(new RegExp(text));
+      expect(element).toBeInTheDocument();
+    });
+
+    const bookElements = screen.getAllByTestId("BookComponent");
+
+    // Check if the length matches the number of books in mockedData
+    expect(bookElements.length).toBe(mockedData.data.length);
+  });
 });
 
 test("renders learn react link with mocking data", () => {
-  render(<App IS_MOCKING_DATA={true} />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<App IS_MOCKING_DATA={IS_MOCKING_DATA} />);
+  const linkElement = screen.getByText(/favorites/i);
+
   expect(linkElement).toBeInTheDocument();
 });
